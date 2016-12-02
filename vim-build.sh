4121118e568be49181e839918481ed756831f2f4
chakku <chakkurai0724@outlook.jp>
2016-12-02 15:10:25 +0900

    add vim-build.sh

---
+++ vim-build.sh
@@ -0,0 +1,14 @@
+./configure \
+--with-features=huge \
+--with-compiledby="chakku" \
+--enable-multibyte \
+--enable-gui=gtk2 \
+--enable-rubyinterp \
+--enable-python3interp \
+--enable-luainterp \
+--with-lua-prefix=/usr \
+--with-luajit \
+--enable-gpm \
+--enable-cscope \
+--enable-fontset \
+--enable-fail-if-missing
