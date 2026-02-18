8da639f3f956c707e8a04863b34c06938746404e
chakku000 <chakkurai0724@outlook.jp>
2026-02-19 01:49:32 +0900

    update antigen configs

--- zsh/conf_antigen.sh
+++ zsh/conf_antigen.sh
@@ -1,7 +1,20 @@
 antigen use oh-my-zsh
 
+# zsh-users/zsh-autosuggestions: 履歴から自動補完
+antigen bundle zsh-users/zsh-autosuggestions
+
+# jeffreytse/zsh-vi-mode: vim-mode
 antigen bundle jeffreytse/zsh-vi-mode
+
+# zsh-vi-modeの後にキーバインドを設定
+function zvm_after_init() {
+  bindkey '^e' autosuggest-accept # ctrl-eで補完
+}
+
+# zdharma-continuum/fast-syntax-highlighting: コマンドに色を付ける
+# (syntax highlightingは最後に読み込む)
 antigen bundle zdharma-continuum/fast-syntax-highlighting
+
 antigen theme romkatv/powerlevel10k
 
 antigen apply
